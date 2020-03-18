a6a33e5ea0781b7235814ed0e329cd41c9f5d277
Kyle Berner <kyleberner14@yahoo.com>
2020-03-17 23:31:49 -0700

    updating js ideas

---
+++ highscore.js
@@ -0,0 +1,2 @@
+/* Take highscore values from local storage and have them displayed in order of highest to lowest
+when user clicks button to return to original page, have it reset the game. */
---
+++ index.js
@@ -0,0 +1,16 @@
+/*When the game begins, the timer goes down
+Game begins on first click
+a question will appear with four potential answers
+answers will be represented by buttons with text
+when an answer is chosen,
+if correct, proceed to next question
+if incorrect, proceed to next question but subtract 10 seconds from the timer
+game ends with last question or end of timer
+display final score
+ask for initials
+when initials are entered, take user to highscore.html
+send score and initials into highscore.html
+sort score in rank of highest to lowerst*/
+
+var timeRemaining = 60;
+var 
